0539234b6cbc27661b5f13e4ffa2f4c6a2962d1b
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-03-27 11:08:09 +0300

    Create repositories for entities

---
+++ src/main/java/fic/writer/domain/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package fic.writer.domain.repository;
+
+import fic.writer.domain.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ArticleRepository extends JpaRepository<Article, UUID> {
+}
