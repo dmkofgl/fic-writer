239455e5eb43bcb0122d7de81ca032065ce27fb9
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-03-27 17:04:21 +0300

    Add annotations

--- src/main/java/fic/writer/domain/service/ActorStateService.java
+++ src/main/java/fic/writer/domain/service/ActorStateService.java
@@ -4,7 +4,6 @@ import fic.writer.domain.entity.ActorState;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
--- src/main/java/fic/writer/domain/service/impl/UserServiceImpl.java
+++ src/main/java/fic/writer/domain/service/impl/UserServiceImpl.java
@@ -6,11 +6,12 @@ import fic.writer.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
