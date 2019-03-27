1a6cf4adeb30ff4c2595a7fa424d899148af36cb
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-03-27 17:34:15 +0300

    Change UUID to long id

--- src/main/java/fic/writer/domain/service/ActorService.java
+++ src/main/java/fic/writer/domain/service/ActorService.java
@@ -4,7 +4,7 @@ import fic.writer.domain.entity.Actor;
 
 import java.util.UUID;
 
-public interface ActorService extends CrudService<Actor, UUID> {
+public interface ActorService extends CrudService<Actor, Long> {
 
 
 }
