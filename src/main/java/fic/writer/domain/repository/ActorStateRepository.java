897e39e47a597ae967de2724029845515d07688a
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-03-27 14:52:44 +0300

    Add findAllByActorId in ActorStateRepository

--- src/main/java/fic/writer/domain/repository/ActorStateRepository.java
+++ src/main/java/fic/writer/domain/repository/ActorStateRepository.java
@@ -1,9 +1,12 @@
 package fic.writer.domain.repository;
 
 import fic.writer.domain.entity.ActorState;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ActorStateRepository extends JpaRepository<ActorState, UUID> {
+    Page<ActorState> findAllByActorId(UUID actorId, Pageable pageable);
 }
