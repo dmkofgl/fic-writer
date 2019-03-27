ca1e5708c42ef3b00e08b278b54afc8543dde3a0
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-03-27 17:19:08 +0300

    Change UUID to long id

--- src/main/java/fic/writer/domain/repository/ActorRepositry.java
+++ src/main/java/fic/writer/domain/repository/ActorRepositry.java
@@ -3,7 +3,5 @@ package fic.writer.domain.repository;
 import fic.writer.domain.entity.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface ActorRepositry extends JpaRepository<Actor, UUID> {
+public interface ActorRepositry extends JpaRepository<Actor, Long> {
 }
--- src/main/java/fic/writer/domain/repository/GenreRepository.java
+++ src/main/java/fic/writer/domain/repository/GenreRepository.java
@@ -2,9 +2,6 @@ package fic.writer.domain.repository;
 
 import fic.writer.domain.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-public interface GenreRepository extends JpaRepository<Genre, UUID> {
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
