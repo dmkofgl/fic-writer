38faeeb5930aeddb46180312b44b75c9087f5d43
Vladislav Barkov <dmkofgl@gmail.com>
2019-03-28 12:30:45 +0300

    Save actorStateDto

--- src/main/java/fic/writer/domain/service/ActorStateService.java
+++ src/main/java/fic/writer/domain/service/ActorStateService.java
@@ -1,15 +1,17 @@
 package fic.writer.domain.service;
 
 import fic.writer.domain.entity.ActorState;
+import fic.writer.domain.entity.dto.ActorStateDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface ActorStateService extends CrudService<ActorState, Long> {
 
     Page<ActorState> findAllByActor(Long id, Pageable pageable);
 
     Optional<ActorState> findForActorByArticle(Long actorId, Long articleId);
+
+    ActorState save(ActorStateDto actorStateDto);
 }
