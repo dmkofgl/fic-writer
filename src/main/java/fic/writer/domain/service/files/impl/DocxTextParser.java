e735029b1b5934b18df81b0957f82d7294525953
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-04-24 12:46:09 +0300

    Allow download and upload files.

---
+++ src/main/java/fic/writer/domain/service/files/impl/DocxTextParser.java
@@ -0,0 +1,28 @@
+package fic.writer.domain.service.files.impl;
+
+import fic.writer.domain.service.files.TextParser;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+//TODO IT read unformatted text
+public class DocxTextParser implements TextParser {
+
+    @Override
+    public String parseFile(MultipartFile file) {
+        String fileContent = "";
+        try {
+            XWPFDocument document = new XWPFDocument(file.getInputStream());
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+            for (int i = 0; i < paragraphs.size(); i++) {
+                fileContent += paragraphs.get(i).getParagraphText();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        return fileContent;
+    }
+}
